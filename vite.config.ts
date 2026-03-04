3705f8202417526f799f4f448e9d2586df152657
Matthew Campbell <m.campbell92@gmail.com>
2026-03-04 19:24:35 +0100

    add base field for deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/react-ts-offices-challenge/",
   plugins: [react(), tailwindcss()],
 });
